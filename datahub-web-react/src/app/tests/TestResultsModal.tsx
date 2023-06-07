00b36c25f8c8db6912d0a335665d559c4fb9d56c
John Joyce <john@acryl.io>
2023-06-07 14:56:02 -0700

    fix(tests): Fixing pagination on Metadata Test results  (#1420)

--- datahub-web-react/src/app/tests/TestResultsModal.tsx
+++ datahub-web-react/src/app/tests/TestResultsModal.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Button, Modal, Tabs } from 'antd';
+import { Modal, Tabs } from 'antd';
 import { EmbeddedListSearchSection } from '../entity/shared/components/styled/search/EmbeddedListSearchSection';
 import { UnionType } from '../search/utils/constants';
 import { TestResultType } from '../../types.generated';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+`;
 
 const StyledModal = styled(Modal)`
     top: 4vh;
@@ -13,9 +19,8 @@ const StyledModal = styled(Modal)`
 const MODAL_WIDTH = '80vw';
 
 const MODAL_BODY_STYLE = {
-    height: '76vh',
     padding: 0,
-    overflow: 'hidden',
+    height: '76vh',
 };
 
 const tabBarStyle = { paddingLeft: 28, paddingBottom: 0, marginBottom: 0 };
@@ -43,39 +48,37 @@ export default function TestResultsModal({
             visible
             width={MODAL_WIDTH}
             title={<>Results - {name}</>}
-            closable={false}
+            closable
             onCancel={onClose}
             bodyStyle={MODAL_BODY_STYLE}
             data-testid="test-results-modal"
-            footer={
-                <Button onClick={onClose} type="text" data-testid="test-results-modal-close-button">
-                    Close
-                </Button>
-            }
+            footer={null}
         >
-            <Tabs
-                tabBarStyle={tabBarStyle}
-                defaultActiveKey={resultType}
-                activeKey={resultType}
-                size="large"
-                onTabClick={(newType) => setResultType(newType as TestResultType)}
-                onChange={(newType) => setResultType(newType as TestResultType)}
-            >
-                <Tabs.TabPane tab={`Passing (${passingCount})`} key={TestResultType.Success} />
-                <Tabs.TabPane tab={`Failing (${failingCount})`} key={TestResultType.Failure} />
-            </Tabs>
-            <EmbeddedListSearchSection
-                defaultShowFilters
-                fixedFilters={{
-                    unionType: UnionType.AND,
-                    filters: [
-                        {
-                            field: resultType === TestResultType.Success ? 'passingTests' : 'failingTests',
-                            values: [urn],
-                        },
-                    ],
-                }}
-            />
+            <Container>
+                <Tabs
+                    tabBarStyle={tabBarStyle}
+                    defaultActiveKey={resultType}
+                    activeKey={resultType}
+                    size="large"
+                    onTabClick={(newType) => setResultType(newType as TestResultType)}
+                    onChange={(newType) => setResultType(newType as TestResultType)}
+                >
+                    <Tabs.TabPane tab={`Passing (${passingCount})`} key={TestResultType.Success} />
+                    <Tabs.TabPane tab={`Failing (${failingCount})`} key={TestResultType.Failure} />
+                </Tabs>
+                <EmbeddedListSearchSection
+                    defaultShowFilters
+                    fixedFilters={{
+                        unionType: UnionType.AND,
+                        filters: [
+                            {
+                                field: resultType === TestResultType.Success ? 'passingTests' : 'failingTests',
+                                values: [urn],
+                            },
+                        ],
+                    }}
+                />
+            </Container>
         </StyledModal>
     );
 }
