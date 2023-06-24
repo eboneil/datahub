28b203d24971877f6958bbd8e38fecb4840c3797
John Joyce <john@acryl.io>
2023-06-23 17:04:04 -0700

    fix(observability): Fix monitors service dockerfile + start script (#1456)

--- docker/datahub-monitors-service/start.sh
+++ docker/datahub-monitors-service/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting datahub monitors service..."
-poetry run uvicorn datahub_monitors.server:app --host 0.0.0.0 --port 9004 ${EXTRA_UVICORN_ARGS:-}
+poetry run uvicorn datahub_monitors.app.server:app --host 0.0.0.0 --port 9004 ${EXTRA_UVICORN_ARGS:-}
