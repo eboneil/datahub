40f1a355ceff13775c7a2d0261c8ddf757a1660e
Ellie O'Neil <oneile729@gmail.com>
2023-09-08 17:08:53 -0700

    to do: figure out schema validation errors for ExtendedPropertiesClass and ExtendedPropertyValueAssignmentClass

--- metadata-ingestion/src/datahub/api/entities/dataset/dataset.py
+++ metadata-ingestion/src/datahub/api/entities/dataset/dataset.py
@@ -1,15 +1,17 @@
-from typing import Dict, List, Optional
+import os
+from typing import Dict, List, Optional, Union
 
 from datahub.api.entities.extendedproperties.extendedproperties import ExtendedProperties
+from datahub.ingestion.extractor.schema_util import avro_schema_to_mce_fields
 import yaml
 import logging
-from datahub.emitter.mce_builder import make_dataset_urn
+from datahub.emitter.mce_builder import make_data_platform_urn, make_dataset_urn
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.ingestion.graph.client import DataHubGraph, get_default_graph
-from datahub.metadata.schema_classes import (DatasetPropertiesClass, ExtendedPropertiesClass, ExtendedPropertyValueAssignmentClass,
+from datahub.metadata.schema_classes import (DatasetPropertiesClass, ExtendedPropertiesClass, ExtendedPropertyValueAssignmentClass, OtherSchemaClass, SchemaMetadataClass,
                                              SubTypesClass, UpstreamClass)
 from datahub.specific.dataset import DatasetPatchBuilder
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Field, validator
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -32,13 +34,13 @@ class Dataset(BaseModel):
     urn: Optional[str]
     description: Optional[str]
     name: Optional[str]
-    # schema_field: Optional[SchemaSpecification] = Field(alias="schema")
+    schema_field: Optional[SchemaSpecification] = Field(alias="schema")
     downstreams: Optional[List[str]]
     terms: Optional[list]
     properties: Optional[Dict[str, str]]
     subtype: Optional[str]
     subtypes: Optional[List[str]]
-    # extended_properties: Optional[Dict[str, str]] = None
+    extended_properties: Optional[Dict[str, Union[str, List[str]]]] = None
 
     @validator("urn", pre=True, always=True)
     def urn_must_be_present(cls, v, values):
@@ -61,6 +63,11 @@ class Dataset(BaseModel):
         if v.startswith("urn:li:dataPlatform:"):
             return v[len("urn:li:dataPlatform:") :]
         return v
+    
+    def make_logical_entity_urn(entity_type: str) -> str:
+        if not entity_type.startswith("urn:li:logicalEntity:"):
+            return f"urn:li:logicalEntity:{entity_type}"
+        return entity_type
 
     def create(file: str):
         emitter: DataHubGraph
@@ -78,21 +85,20 @@ class Dataset(BaseModel):
                         ),
                     )
                     emitter.emit_mcp(mcp)
-                    # print('SCHEMA: ', dataset.schema_field)
-                    # with open(dataset.schema_field.file, "r") as fp:
-                    #     schema_string = fp.read()
-                    #     schema_metadata = SchemaMetadataClass(
-                    #         schemaName="test",
-                    #         platform=make_data_platform_urn(dataset.platform),
-                    #         version=0,
-                    #         hash="",
-                    #         platformSchema=OtherSchemaClass(rawSchema=schema_string),
-                    #         fields=avro_schema_to_mce_fields(schema_string),
-                    #     )
-                    #     mcp = MetadataChangeProposalWrapper(
-                    #         entityUrn=dataset.urn, aspect=schema_metadata
-                    #     )
-                    #     emitter.emit_mcp(mcp)
+                    with open(dataset.schema_field.file, "r") as fp:
+                        schema_string = fp.read()
+                        schema_metadata = SchemaMetadataClass(
+                            schemaName=dataset.name,
+                            platform=make_data_platform_urn(dataset.platform),
+                            version=0,
+                            hash="",
+                            platformSchema=OtherSchemaClass(rawSchema=schema_string),
+                            fields=avro_schema_to_mce_fields(schema_string),
+                        )
+                        mcp = MetadataChangeProposalWrapper(
+                            entityUrn=dataset.urn, aspect=schema_metadata
+                        )
+                        emitter.emit_mcp(mcp)
 
                     if dataset.subtype or dataset.subtypes:
                         mcp = MetadataChangeProposalWrapper(
@@ -107,23 +113,20 @@ class Dataset(BaseModel):
                         )
                         emitter.emit_mcp(mcp)
                     
-                    # if dataset.extended_properties:
-                    #     print("EP: ", dataset.extended_properties)
-                    #     for property in dataset.extended_properties.items():
-                    #         print('key: ', property.key())
-                    #         print('value: ', property.value())
-                    #     mcp = MetadataChangeProposalWrapper(
-                    #         entityUrn=dataset.urn,
-                    #         aspect=ExtendedPropertiesClass(
-                    #             properties=[
-                    #                 ExtendedPropertyValueAssignmentClass(
-                    #                     propertyUrn=f"urn:li:extendedProperty:{property.key}", value=property.value
-                    #                 )
-                    #                 for property in dataset.extended_properties.items()
-                    #             ]
-                    #         ),
-                    #     )
-                    #     emitter.emit_mcp(mcp)
+                    if dataset.extended_properties:
+                        mcp = MetadataChangeProposalWrapper(
+                            entityUrn=dataset.urn,
+                            aspect=ExtendedPropertiesClass(
+                                properties=[
+                                    ExtendedPropertyValueAssignmentClass(
+                                        propertyUrn=f"urn:li:extendedProperty:{property[0]}",
+                                        value=property[1],
+                                    )
+                                    for property in dataset.extended_properties.items()
+                                ]
+                            ),
+                        )
+                        emitter.emit_mcp(mcp)
 
                     if dataset.downstreams:
                         for downstream in dataset.downstreams:
--- metadata-ingestion/src/datahub/api/entities/extendedproperties/extendedproperties.py
+++ metadata-ingestion/src/datahub/api/entities/extendedproperties/extendedproperties.py
@@ -6,6 +6,7 @@ from datahub.ingestion.graph.client import DataHubGraph, get_default_graph
 from datahub.metadata.schema_classes import ExtendedPropertyDefinitionClass
 
 from datahub.configuration.common import ConfigModel
+from pydantic import validator
 import yaml
 
 logging.basicConfig(level=logging.INFO)
@@ -14,26 +15,32 @@ logger = logging.getLogger(__name__)
 
 class ExtendedProperties(ConfigModel):
 
-    id: Optional[str] = None
-    fully_qualified_name: Optional[str] = None
-    type: Optional[str] = None
-    description: Optional[str] = None
-    display_name: Optional[str] = None
-    entity_types: Optional[List[str]] = None
-    cardinality: Optional[str] = None
-    allowed_values: Optional[dict] = None
+    id: Optional[str]
+    urn: Optional[str]
+    fully_qualified_name: Optional[str]
+    type: Optional[str]
+    description: Optional[str]
+    display_name: Optional[str]
+    entity_types: Optional[List[str]]
+    cardinality: Optional[str]
+    allowed_values: Optional[List[dict[str, str]]]
 
-    property
-    def urn(self) -> str:
-        if self.id.startswith("urn:li:extendedProperty:"):
-            return self.id
-        else:
-            return f"urn:li:extendedProperty:{self.id}"
+    @validator("urn", pre=True, always=True)
+    def urn_must_be_present(cls, v, values):
+        if not v:
+            assert "id" in values, "id must be present if urn is not"
+            return f"urn:li:extendedProperty:{values['id']}"
+        return v
         
     def make_logical_type_urn(type: str) -> str:
         if not type.startswith("urn:li:logicalType:"):
             return f"urn:li:logicalType:{type}"
         return type
+
+    def make_logical_entity_urn(entity_type: str) -> str:
+        if not entity_type.startswith("urn:li:logicalEntity:"):
+            return f"urn:li:logicalEntity:{entity_type}"
+        return entity_type
 
     def create(file: str):
         emitter: DataHubGraph
@@ -49,7 +56,7 @@ class ExtendedProperties(ConfigModel):
                             valueType=ExtendedProperties.make_logical_type_urn(extendedproperty.type),
                             displayName=extendedproperty.display_name,
                             description=extendedproperty.description,
-                            entity_types=extendedproperty.entity_types,
+                            entityTypes=[ExtendedProperties.make_logical_entity_urn(entity_type) for entity_type in extendedproperty.entity_types],
                             cardinality=extendedproperty.cardinality,
                             allowedValues=extendedproperty.allowed_values,
                         ),
