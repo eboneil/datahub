a0bbd2f7974219ba90a889eaa8e2e5f975a3c416
Ellie O'Neil <oneile729@gmail.com>
2023-09-08 17:27:01 -0700

    clean up a bit

--- metadata-ingestion/src/datahub/cli/specific/extendedproperties_cli.py
+++ metadata-ingestion/src/datahub/cli/specific/extendedproperties_cli.py
@@ -22,50 +22,6 @@ def extendedproperties() -> None:
     pass
 
 
-# def mutate(file: Path, validate_assets: bool, external_url: str, upsert: bool) -> None:
-#     """Update or Upsert extended properties in DataHub"""
-
-#     config_dict = load_file(pathlib.Path(file))
-#     id = config_dict.get("id") if isinstance(config_dict, dict) else None
-#     with get_default_graph() as graph:
-#         dataset: ExtendedProperties = ExtendedProperties.from_yaml(file, graph)
-#         external_url_override = (
-#             external_url
-#             or os.getenv("DATAHUB_EXTENDED_PROPERTIES_EXTERNAL_URL")
-#             or dataset.external_url
-#         )
-#         dataset.external_url = external_url_override
-#         if upsert and not graph.exists(dataset.urn):
-#             logger.info(f"Dataset {dataset.urn} does not exist, will create.")
-#             upsert = False
-
-#         if validate_assets and dataset.assets:
-#             missing_assets = []
-#             for asset in dataset.assets:
-#                 try:
-#                     assert graph.exists(asset)
-#                 except Exception as e:
-#                     logger.debug("Failed to validate existence", exc_info=e)
-#                     missing_assets.append(asset)
-#             if missing_assets:
-#                 for a in missing_assets:
-#                     click.secho(f"Asset: {a} doesn't exist on DataHub", fg="red")
-#                 click.secho(
-#                     "Aborting update due to the presence of missing assets in the yaml file. Turn off validation of assets using the --no-validate-assets option if you want to proceed.",
-#                     fg="red",
-#                 )
-#                 raise click.Abort()
-#         try:
-#             for mcp in dataset.generate_mcp(upsert=upsert):
-#                 graph.emit(mcp)
-#             click.secho(f"Update succeeded for urn {dataset.urn}.", fg="green")
-#         except Exception as e:
-#             click.secho(
-#                 f"Update failed for id {id}. due to {e}",
-#                 fg="red",
-#             )
-
-
 @extendedproperties.command(
     name="upsert",
 )
